0b38a0cecaaa24c0514efa356238a78076d06e47
satayamyadav <satyamyadav@live.com>
2015-06-21 02:31:02 +0530

    fix btn toggle

--- js/init.js
+++ js/init.js
@@ -3,6 +3,12 @@
 
     $('.modal-trigger').leanModal();
     $('.button-collapse').sideNav();
+    var $share = $('.social-buttons');
+    var $fixBtn = $('.fix-btn');
+
+    $fixBtn.on('click', function(ev) {
+      $share.toggle();
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
